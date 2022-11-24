7d821b03c103bc58bed77af77f3212ad6ffafcb4
TaeWon Park <thisis_tw94@naver.com>
2022-11-24 21:01:31 +0900

    lecture: react

---
+++ blog_with_Node_and_React_Youtube-main/React/init/src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import './homepage.css'
+
+export default function Homepage() {
+  return (
+    <>
+      <div className='home'>Homepage</div>
+    </>
+  )
+}
--- shoppingmall/my-app/src/components/Card/ProductImage/ProductImage.jsx
+++ shoppingmall/my-app/src/components/Card/ProductImage/ProductImage.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export default function ProductImage({thumbnailImg, productName}) {
-    // console.log(thumbnailImg)
-    const url = 'https://test.api.weniv.co.kr/' + thumbnailImg
-    return (
-        <img src={url} alt={productName}/>
-    )
+  // console.log(thumbnailImg)
+  const url = 'https://test.api.weniv.co.kr/' + thumbnailImg
+  return (
+    <img src={url} alt="products"/>
+  )
 }
