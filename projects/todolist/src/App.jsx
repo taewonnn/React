3f6c164364b9bd262a366c594401937d49fc27e3
taewonnn <thisis_tw94@naver.com>
2025-08-26 11:09:48 +0900

    add: todolist template

--- projects/todolist/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      <div></div>
-    </div>
-  );
-}
-
-export default App;
--- projects/todolist/vite.config.ts
+++ projects/todolist/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3003,
+    host: true,
     open: true,
   },
 });
