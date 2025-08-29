e3698e764a0ac39e95c05bccd1a62f40a4b85eab
Turbobot <turbobot@vercel.com>
2025-08-29 14:43:47 +0900

    feat(create-turbo): apply official-starter transform

--- projects/todolist/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: ['../../configs/eslint/eslint.config.base.js'],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-  },
-};
--- use-node22.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source ~/.nvm/nvm.sh
-nvm use 22
-echo "Node.js 22 활성화됨!"
