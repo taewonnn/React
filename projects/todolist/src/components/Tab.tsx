7c6820dd49dc16502c24a31a300611950d65a0d9
taewonnn <thisis_tw94@naver.com>
2025-09-08 10:00:00 +0900

    feat: tab status

--- projects/todolist/src/components/Tab.tsx
+++ projects/todolist/src/components/Tab.tsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
+
+type TabType = 'All' | 'Active' | 'Completed';
+
 export default function Tab() {
-  
+  const tabItems: TabType[] = ['All', 'Active', 'Completed'];
+
+  const [tab, setTab] = useState<TabType>('All');
+
+  const handleChangeTab = (status: TabType) => {
+    console.log(status);
+    setTab(status);
+  };
+
   return (
-    <ul className="flex gap-2 justify-center">
-      <button>All</button>
-      <button>Active</button>
-      <button>Completed</button>
+    <ul className="flex gap-3 justify-center">
+      {tabItems.map(tabItem => (
+        <button
+          key={tabItem}
+          onClick={() => handleChangeTab(tabItem)}
+          className={`${tabItem == tab ? 'font-bold bg-blue-500 text-white' : ''}`}
+        >
+          {tabItem}
+        </button>
+      ))}
     </ul>
   );
 }
