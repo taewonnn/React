87842bcaaf03cb2b67c4876c2c07989844f4f54f
taewonnn <thisis_tw94@naver.com>
2025-09-12 11:15:29 +0900

    immer

--- apps/prac/src/components/course/CourseForm.jsx
+++ apps/prac/src/components/course/CourseForm.jsx
@@ -5,6 +5,10 @@ export default function CourseForm() {
   const [form, setForm] = useState({
     title: '리액트 강의',
     description: '리액트 기초부터 실전까지!',
+    info: {
+      level: 1,
+      skill: 'react',
+    },
   });
 
   function handleCourseForm(e) {
@@ -19,16 +23,61 @@ export default function CourseForm() {
       [e.target.name]: e.target.value, // [e.target.name]은 변수명이 됨
     });
   };
+
+  //skill
+  const handleSkillChange = e => {
+    setForm({
+      ...form,
+      info: {
+        ...form.info, // 중첩된 객체를 유지하면서 skill 값만 변경
+        skill: e.target.value,
+      },
+    });
+  };
+
+  // level
+  const handleLevelChange = e => {
+    console.log('e.target: ', e.target);
+    setForm({
+      ...form,
+      info: {
+        ...form.info, // 중첩된 객체를 유지하면서 level 값만 변경
+        level: e.target.value,
+      },
+    });
+  };
+
   return (
     <Card title='강의 등록'>
       <form style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }} onSubmit={handleCourseForm}>
         <input type='text' name='title' placeholder='강의 제목' value={form.title} onChange={handleChange} />
         <input type='text' name='description' placeholder='강의 한줄 설명' value={form.description} onChange={handleChange} />
+
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <label htmlFor='skill' style={{ width: '100px' }}>
+            skill
+          </label>
+          <input type='text' name='skill' id='skill' value={form.info.skill} onChange={handleSkillChange} />
+        </div>
+
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <label htmlFor='level' style={{ width: '100px' }}>
+            level
+          </label>
+          <select name='level' id='level' value={form.info.level} onChange={handleLevelChange}>
+            <option value='0'>초급</option>
+            <option value='1'>중급</option>
+            <option value='2'>고급</option>
+          </select>
+        </div>
+
         <input type='submit' value='등록' />
         {(form.title || form.description) && (
           <div style={{ marginTop: '16px', padding: '16px', backgroundColor: '#eee', borderRadius: '6px' }}>
             {form.title && <p>제목 - {form.title}</p>}
             {form.description && <p>설명 - {form.description}</p>}
+            {form.info.skill && <p>skill - {form.info.skill}</p>}
+            {form.info.level && <p>level - {form.info.level}</p>}
           </div>
         )}
       </form>
