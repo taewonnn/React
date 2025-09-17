ab8d37374d8af3f2b7a563a93de7a33291bc533f
taewonnn <thisis_tw94@naver.com>
2025-09-17 10:00:00 +0900

    test

--- apps/prac/src/main.jsx
+++ apps/prac/src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppCounter from './AppCounter.jsx';
-import App from './AppCounter.jsx';
+import App from './AppMovingDot.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
