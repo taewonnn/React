3cec0ae464a90e6ee052edd60d4fb059293aaa01
taewonnn <thisis_tw94@naver.com>
2025-09-22 10:00:00 +0900

    todo

--- apps/prac/src/AppTodo.jsx
+++ apps/prac/src/AppTodo.jsx
@@ -3,14 +3,27 @@ import './App.css';
 import TodoList from './components/todo/TodoList';
 
 function AppTodo() {
+  const [todoText, setTodoText] = useState('');
   const [todos, setTodos] = useState([
-    { id: 0, label: 'HTML&CSS 공부하기' },
-    { id: 1, label: '자바스크립트 공부하기' },
+    { id: 0, text: 'HTML&CSS 공부하기' },
+    { id: 1, text: '자바스크립트 공부하기' },
   ]);
+
+  const handleAddTodo = () => {
+    const nextId = todos.length + Math.floor(Math.random() * 10000);
+    setTodos([...todos, { id: nextId, text: todoText }]);
+  };
+
+  const handleChangeTodoText = e => {
+    setTodoText(e.target.value);
+  };
 
   return (
     <div>
       <h2>할일목록</h2>
+      <input type='text' placeholder='할일을 입력하세요.' onChange={handleChangeTodoText} />
+      <button onClick={handleAddTodo}>추가</button>
+      <div>Preview: {todoText}</div>
       <TodoList todos={todos} />
     </div>
   );
