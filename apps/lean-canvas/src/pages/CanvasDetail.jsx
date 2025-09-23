ac63a0234fd106e085b8f47fa988068ad73e9e2f
taewonnn <thisis_tw94@naver.com>
2025-09-23 15:26:47 +0900

    feat: canvas ui

--- apps/lean-canvas/src/pages/CanvasDetail.jsx
+++ apps/lean-canvas/src/pages/CanvasDetail.jsx
@@ -1,9 +1,11 @@
 import CanvasTitle from '../components/CanvasTitle';
+import LeanCanvas from '../components/LeanCanvas';
 
 export default function CanvasDetail() {
   return (
     <div>
       <CanvasTitle />
+      <LeanCanvas />
     </div>
   );
 }
