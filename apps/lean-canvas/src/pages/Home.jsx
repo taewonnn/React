7ed8f9f3ca5507d941bdaef9d4fe5972a993b92f
taewonnn <thisis_tw94@naver.com>
2025-09-23 10:42:58 +0900

    chore

--- apps/lean-canvas/src/pages/Home.jsx
+++ apps/lean-canvas/src/pages/Home.jsx
@@ -24,10 +24,13 @@ function Home() {
   return (
     <div className='container mx-auto px-4 py-16'>
       <div className='mb-6 flex flex-col sm:flex-row items-center justify-between'>
+        {/* 검색 UI */}
         <SearchBar search={search} handleSearch={handleSearch} />
+        {/* 뷰 토글 UI */}
         <ViewToggle setIsGridView={setIsGridView} isGridView={isGridView} />
       </div>
 
+      {/* 캔버스 리스트 UI */}
       <CanvasList filteredCardData={filteredCardData} isGridView={isGridView} search={search} />
     </div>
   );
