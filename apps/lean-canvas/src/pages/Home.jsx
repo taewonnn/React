f949372f94a8cbf7dbca9c4aad96280a93b8cb08
taewonnn <thisis_tw94@naver.com>
2025-09-23 17:40:15 +0900

    chore

--- apps/lean-canvas/src/pages/Home.jsx
+++ apps/lean-canvas/src/pages/Home.jsx
@@ -23,12 +23,22 @@ function Home() {
   };
 
   const filteredData = data.filter(item => item.title.toLowerCase().includes(searchText.toLowerCase()));
+
+  // ✅ debugger
+  // const filteredCardData = cardData.filter(card => {
+  //   debugger;
+  //   return card.title.toLowerCase().includes(search.toLowerCase());
+  // });
+
   return (
     <div className='container mx-auto px-4 py-16'>
       <div className='mb-6 flex flex-col sm:flex-row items-center justify-between'>
+        {/* 검색 UI */}
         <SearchBar searchText={searchText} setSearchText={setSearchText} />
+        {/* 뷰 토글 UI */}
         <ViewToggle isGridView={isGridView} setIsGridView={setIsGridView} />
       </div>
+      {/* 캔버스 리스트 UI */}
       <CanvasList filteredData={filteredData} isGridView={isGridView} searchText={searchText} onDeleteItem={handleDeleteItem} />
     </div>
   );
