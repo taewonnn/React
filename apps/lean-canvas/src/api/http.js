331c0d95c9c26ee93d15b24e57e32765312457df
taewonnn <thisis_tw94@naver.com>
2025-09-25 18:19:14 +0900

    chore: gitignore

--- apps/lean-canvas/src/api/http.js
+++ apps/lean-canvas/src/api/http.js
@@ -5,4 +5,11 @@ function create(baseURL, options) {
   return instance;
 }
 
-export const canvases = create('http://localhost:8000/canvases/');
+console.log('MODE:', import.meta.env.MODE); // MODE: development
+console.log('VITE_BASE_URL:', import.meta.env.VITE_API_BASE_URL);
+
+export const canvases = create(`${import.meta.env.VITE_API_BASE_URL}/canvases`);
+// export const canvases = create('https://json-server-vercel-6yu8.vercel.app/canvases');
+
+// https://json-server-vercel-6yu8.vercel.app/canvases
+// http://localhost:8000/canvases/
