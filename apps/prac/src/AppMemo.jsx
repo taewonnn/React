6a5d38eacf67d3a8f01de8382e36c7b1a03f24db
taewonnn <thisis_tw94@naver.com>
2025-10-16 10:00:00 +0900

    useCallback

--- apps/prac/src/AppMemo.jsx
+++ apps/prac/src/AppMemo.jsx
@@ -1,6 +1,6 @@
-import { useState, memo, useMemo } from 'react';
+import { useState, memo, useMemo, useCallback } from 'react';
 // 메모이제이션이 적용되지 않은 컴포넌트
-const RegularComponent = ({ count, items }) => {
+const RegularComponent = ({ count, items = [], onCount }) => {
   console.log('RegularComponent 렌더링');
 
   return (
@@ -12,13 +12,14 @@ const RegularComponent = ({ count, items }) => {
           <li key={item.id}>{item.text}</li>
         ))}
       </ul>
+      <button onClick={onCount}>카운트 증가</button>
     </fieldset>
   );
 };
 
 // 메모이제이션이 적용된 컴포넌트
 // 컴포넌트를 메모함수 이용해서 메모이제이션 적용
-const MemoizedComponent = memo(({ count, items }) => {
+const MemoizedComponent = memo(({ count, items = [], onCount }) => {
   console.log('MemoizedComponent 렌더링');
   return (
     <fieldset>
@@ -29,6 +30,7 @@ const MemoizedComponent = memo(({ count, items }) => {
           <li key={item.id}>{item.text}</li>
         ))}
       </ul>
+      <button onClick={onCount}>카운트 증가</button>
     </fieldset>
   );
 });
@@ -56,14 +58,22 @@ export default function AppMemo() {
     ];
   }, []);
 
+  // ‼️ 함수도 obj와 마찬가지로 참조값이 변경되어 컴포넌트가 리렌더링 됨
+  // useCallback을 사용해서 성능 최적화를 함
+  // 이 함수는 컴포넌트가 읽히면 한번만 실행되고 이후에는 실행되지 않음
+  // 그래서 성능 최적화를 할 수 있음
+  const handleCount = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
+
   return (
     <div>
       <h2>컴포넌트 메모이제이션</h2>
       <button onClick={() => setCount(count + 1)}>카운트 증가</button>
       <button onClick={() => setOtherState(otherState + 1)}>기타 상태 변경</button>
       <hr />
-      <RegularComponent count={count} items={courses} />
-      <MemoizedComponent count={count} items={courses} />
+      <RegularComponent count={count} items={courses} onCount={handleCount} />
+      <MemoizedComponent count={count} items={courses} onCount={handleCount} />
     </div>
   );
 }
